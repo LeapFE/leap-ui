70a7cfd98fe9422ce085ba3f2311dcd5cdc122f2
front-end-captain <18810940932@163.com>
2020-10-14 10:04:34 +0800

    refactor(tabs): refactor tabs component

--- components/tabs/index.tsx
+++ components/tabs/index.tsx
@@ -7,35 +7,37 @@ import * as AntdTabsInterface from "antd/es/tabs";
 import "./style";
 
 type childProps = {
-  key?: number;
-  renderContent?: (key: number, child: any) => ReactNode;
-  tabKey: string;
+  key: string;
+  renderContent?: (key: string, child: childProps) => ReactNode;
+  tab?: string;
 };
+
 export interface TabsProps extends AntdTabsInterface.TabsProps {
-  datas?: childProps[];
-  tabKey?: string;
-  renderContent?: (key: number | undefined, child: childProps) => ReactNode;
+  data?: childProps[];
+  renderContent?: (key: string, child: childProps) => ReactNode;
 }
 
 class Tabs extends Component<TabsProps> {
   static TabPane: typeof AntdTabs.TabPane;
 
   render() {
-    const { className, datas, tabKey = "tab", renderContent } = this.props;
-    if (datas && datas.length) {
+    const { className, data, renderContent } = this.props;
+    if (Array.isArray(data) && data.length > 0) {
       return (
-        <Tabs className={ClassNames("fl_tabs", className)} {...this.props}>
-          {datas.map((child, key) => (
-            <AntdTabs.TabPane
-              tab={child[tabKey]}
-              key={(child.key === undefined ? key : child.key).toString()}
-            >
-              {child.renderContent || (renderContent ? renderContent(child.key, child) : "")}
+        <AntdTabs className={ClassNames("fl_tabs", className)} {...this.props}>
+          {data.map((item, i) => (
+            <AntdTabs.TabPane tab={item.tab || `Tab${i + 1}`} key={item.key}>
+              {typeof item.renderContent === "function"
+                ? item.renderContent(item.key, item)
+                : typeof renderContent === "function"
+                ? renderContent(item.key, item)
+                : null}
             </AntdTabs.TabPane>
           ))}
-        </Tabs>
+        </AntdTabs>
       );
     }
+
     return <AntdTabs {...this.props} className={ClassNames("fl_tabs", className)} />;
   }
 }
