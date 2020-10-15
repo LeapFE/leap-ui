e0f4cca2cbe39a8fd99c532fe8ecf1e2b143f6b4
front-end-captain <18810940932@163.com>
2020-10-15 10:17:04 +0800

    chore: rename toolTip component dir

---
+++ components/toolTip/index.tsx
@@ -0,0 +1,5 @@
+import { Tooltip as AntdTooltip } from "antd";
+// import * as AntdTooltipInterface from "antd/es/toolTip";
+import "./style";
+// export { AntdTooltip, AntdTooltipInterface };
+export default AntdTooltip;
